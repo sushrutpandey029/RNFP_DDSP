4d4abccf8a5f8f3bac5b7067c75c6a9d3ba8cca9
Vivek Kumar Pandit <154490785+Vivekkr277@users.noreply.github.com>
2025-02-11 10:48:08 +0530

    Update MySql_Connect.js

--- DB_Connection/MySql_Connect.js
+++ DB_Connection/MySql_Connect.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
 const sequelize  = new Sequelize('milleniancecom_ddsp_app','milleniancecom_ddspapp','@$e$4~bzK5SS',{
-    host: '68.178.173.163',  // Replace with the actual hostname
+    host: '127.0.0.1',  // Replace with the actual hostname
   dialect: 'mysql',
   port: 3306  // Default MySQL port
 });
